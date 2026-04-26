0b68163b454570d06918087620936bcb13ccb17a
PhucLH <phuclhde@gmail.com>
2026-04-26 10:12:21 +0700

    feat: update response message v2

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
-    def DO_GET(self):
+    def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from ArgoCD!")
+        self.wfile.write(b"Hello from ArgoCD - v2!")  # ← đổi message
 
 HTTPServer(("", 8080), Handler).serve_forever()
